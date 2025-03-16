c3a5eb80cdfed1cf29c4732b7cd627db869d5ded
volcano2906 <34382083+volcano2906@users.noreply.github.com>
2025-03-16 15:19:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ uploaded_files = st.file_uploader("CSV dosyanızı yükleyin", type=["csv"], acc
 # Anahtar kelime hacmi 5 olanları filtreleme seçeneği
 drop_low_volume = st.checkbox("Exclude Keywords with Volume 5")
 drop_rank_more = st.checkbox("Exclude Keywords with Rank More Than 11")
+drop_rank_less = st.checkbox("Exclude Keywords with Rank Less Than 11")
 
 def update_rank(rank):
     try:
@@ -63,6 +64,9 @@ if uploaded_files:
 
     if drop_rank_more:
         df = df[df["Rank"] < 11]
+
+    if drop_rank_less:
+        df = df[df["Rank"] > 11]
     
     # Rank değerlerini sayıya çevir ve puan hesapla
     df["Rank"] = df["Rank"].fillna("250").astype(str)
