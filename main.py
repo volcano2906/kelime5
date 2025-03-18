b5af09d0d3a3f47093622f00b0c6533ab9c3eb19
volcano2906 <34382083+volcano2906@users.noreply.github.com>
2025-03-18 20:52:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,9 +69,7 @@ if uploaded_files:
         df = df[df["Rank"] > 11]
     
     # Rank değerlerini sayıya çevir ve puan hesapla
-    #df["Rank"] = df["Rank"].fillna("250").astype(str)
-    df["Rank"] = pd.to_numeric(df["Rank"], errors='coerce')  # Sayıya çevir
-    df["Rank"] = df["Rank"].fillna(250).astype(int)  # NaN'ları 250 yap, integer formatına çevir
+    df["Rank"] = df["Rank"].fillna("250").astype(str)
     df["Score"] = df["Rank"].apply(update_rank)
     
     # Eksik kelimeleri bul
