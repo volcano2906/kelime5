db6405137cd1131d0006f1b9aa116b2c6be3046d
volcano2906 <34382083+volcano2906@users.noreply.github.com>
2025-03-25 00:30:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -344,12 +344,11 @@ if uploaded_files:
         
         if target_app_id in app_results:
             existing_app_words = set(re.split(r'[,\s]+', app_results[target_app_id].lower()))
-            st.write(existing_app_words)
             existing_app_words = {w for w in existing_app_words if w and w not in stop_words}
         
-        st.write(existing_app_words)
         # Remove known words from result
         new_common_words = sorted(common_words - existing_app_words)
+        st.write(new_common_words)
         
         # Display
         if new_common_words:
